dd2a60941c6ed1f935bab72d5a9200aca98b27ad
Shintaro Okamura <48118431+okash1n@users.noreply.github.com>
2022-11-06 23:06:48 +0900

    initial commit

---
+++ pages/blog.tsx
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout";
+
+const Blog = () => {
+  return <Layout title="Blog">Blog Page</Layout>;
+};
+
+export default Blog;
---
+++ pages/contact.tsx
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout";
+
+const Contact = () => {
+  return <Layout title="Contact">Contact Page</Layout>;
+};
+
+export default Contact;
